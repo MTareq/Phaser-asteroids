1fd326bc97c1cd31ec372abcd46af8386124c894
mtareq <m.tareq89@gmail.com>
2015-11-28 19:26:29 +0200

    Physics, and Controls

--- js/game.js
+++ js/game.js
@@ -17,17 +17,26 @@ var graphicAssets = {
 
 var gameState = function(game){
     this.shipSprite;
+
+    this.key_left;
+    this.key_right;
+    this.key_thrust;
 };
 
 var shipProperties = {
     startX: gameProperties.screenWidth * 0.5,
     startY: gameProperties.screenHight * 0.5,
+    acceleration: 300,
+    drag: 100,
+    maxVelocity: 300,
+    angularVelocity: 200,
 };
 
 
 gameState.prototype = {
      
     preload: function(){
+
         game.load.image(graphicAssets.asteroidLarge.name, graphicAssets.asteroidLarge.URL);
         game.load.image(graphicAssets.asteroidMedium.name, graphicAssets.asteroidMedium.URL);
         game.load.image(graphicAssets.asteroidSmall.name, graphicAssets.asteroidSmall.URL);
@@ -37,19 +46,58 @@ gameState.prototype = {
 
     create: function(){
         this.initGraphics();
+        this.initPhysics();
+        this.initKeyboard();
     
     },
 
-    update: function(){},
+    update: function(){
+        this.checkPlayerInput()
+    
+    },
 
     initGraphics: function(){
+
         this.shipSprite = game.add.sprite(shipProperties.startX, shipProperties.startY, graphicAssets.ship.name);
         this.shipSprite.angle = -90;
         this.shipSprite.anchor.set(0.5, 0.5);
     },
 
-};
+    initPhysics: function(){
 
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.enable(this.shipSprite, Phaser.Physics.ARCADE);
+
+        this.shipSprite.body.drag.set(shipProperties.drag);
+        this.shipSprite.body.maxVelocity.set(shipProperties.maxVelocity);
+    },
+
+    initKeyboard: function(){
+    
+        this.key_left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.key_right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        this.key_thrust = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    
+    },
+
+    checkPlayerInput:function(){
+        
+        if (this.key_left.isDown){
+            this.shipSprite.body.angularVelocity = -shipProperties.angularVelocity;
+        }else if (this.key_right.isDown){
+            this.shipSprite.body.angularVelocity = shipProperties.angularVelocity;
+        }else{
+            this.shipSprite.body.angularVelocity = 0;
+        }
+
+        if (this.key_thrust.isDown){
+            game.physics.arcade.accelerationFromRotation(this.shipSprite.rotation, shipProperties.acceleraion, this.shipSprite.body.acceleration);
+        }else{
+            this.shipSprite.body.acceleration.set(0); 
+        }
+    
+    },
+}
 var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHight, Phaser.AUTO, 'gameDiv');
 game.state.add(states.game, gameState);
 game.state.start(states.game);
